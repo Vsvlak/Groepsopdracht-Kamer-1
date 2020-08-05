7a5d94ed3ac1881cf970b381bfc25362a8eecf3c
Lisia <lisia.smienk@ziggo.nl>
2020-08-05 14:03:48 +0200

    Lege klas aanmaken

--- cijferSysteem/src/main/java/com/cs/cijferSysteem/controller/VakService.java
+++ cijferSysteem/src/main/java/com/cs/cijferSysteem/controller/VakService.java
@@ -4,7 +4,6 @@ import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import com.cs.cijferSysteem.domein.Vak;
 

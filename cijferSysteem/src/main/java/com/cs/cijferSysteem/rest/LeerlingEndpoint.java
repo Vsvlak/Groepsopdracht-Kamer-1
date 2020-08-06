f9c327f9963a2f27725651ceb86bead3e04765b9
vsvlak <vsvlak@gmail.com>
2020-08-06 16:55:41 +0200

    toetsen aanmaken

--- cijferSysteem/src/main/java/com/cs/cijferSysteem/rest/LeerlingEndpoint.java
+++ cijferSysteem/src/main/java/com/cs/cijferSysteem/rest/LeerlingEndpoint.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.cs.cijferSysteem.controller.LeerlingService;
 import com.cs.cijferSysteem.domein.Leerling;
 
---
+++ cijferSysteem/src/main/resources/static/ToetsOverzicht.js
@@ -0,0 +1,39 @@
+function laatToetsenZien() {
+    document.getElementById("tabel").innerHTML = "";
+
+    let xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 3) {
+            var info = JSON.parse(this.responseText);
+            document.getElementById("tabel").innerHTML += "<tr><td><b>Datum</b></td><td><b>Tijd</b></td><td><b>Vak</b></td></tr>"
+
+            for (var x = 0; x < info.length; x++) {
+                document.getElementById("tabel").innerHTML += "<tr>" +
+                    "<td>" + info[x].datum + "</td>" +
+                    "<td>" + info[x].tijd + "</td>" +
+                    "<td>" + info[x].vak + "</td>" +
+                    "<td><img src='EditButton.png' class='editB' id=editButton" + x + " style='height:20px;width20px;'></td>" +
+                    "</tr>";
+            }
+        }
+
+    }
+    xhr.open("GET", "http://localhost:8082/toetsOverzicht", true);
+    xhr.send();
+}
+
+    function maakToetsAan(){
+        var datum = document.getElementById("datumInput").value;
+        var vak = document.getElementById("vakInput").value;
+        var tijd = document.getElementById("tijdInput").value;
+        var toets = '{"datum":"'+datum+'","tijd":"'+tijd+'","vak":"'+vak+'"}';
+        postData(toets);
+    }
+
+    function postData(toets){
+        var xhttp = new XMLHttpRequest();
+        xhttp.open("POST", "http://localhost:8082/api/maakToets", true);
+        xhttp.setRequestHeader("Content-type", "application/json");
+        xhttp.send(toets);
+    }
+

47d51ca3bda681510df549c8050b35197b823600
Lisia <lisia.smienk@ziggo.nl>
2020-08-06 13:37:56 +0200

    klasendpoint

--- cijferSysteem/src/main/java/com/cs/cijferSysteem/rest/KlasEndpoint.java
+++ cijferSysteem/src/main/java/com/cs/cijferSysteem/rest/KlasEndpoint.java
@@ -1,8 +1,6 @@
 package com.cs.cijferSysteem.rest;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;

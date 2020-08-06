ad85094bee7eb8c83c885ab6c85c8cff96e28864
Lisia <lisia.smienk@ziggo.nl>
2020-08-06 13:26:21 +0200

    update klas en leerling

--- cijferSysteem/src/main/java/com/cs/cijferSysteem/controller/KlasService.java
+++ cijferSysteem/src/main/java/com/cs/cijferSysteem/controller/KlasService.java
@@ -1,11 +1,14 @@
 package com.cs.cijferSysteem.controller;
 
+import java.util.Optional;
+
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.cs.cijferSysteem.domein.Klas;
+import com.cs.cijferSysteem.domein.Leerling;
 
 @Service
 @Transactional
@@ -20,5 +23,9 @@ public class KlasService {
 
 	public Iterable<Klas> laatKlassenZien(){
 		return kr.findAll();
+	}
+	
+	public Optional<Klas> getKlasById(Long id) {
+		return kr.findById(id);
 	}
 }
--- cijferSysteem/src/main/java/com/cs/cijferSysteem/domein/Vak.java
+++ cijferSysteem/src/main/java/com/cs/cijferSysteem/domein/Vak.java
@@ -5,8 +5,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-//Author: Lisia
-
 @Entity
 public class Vak {
 	

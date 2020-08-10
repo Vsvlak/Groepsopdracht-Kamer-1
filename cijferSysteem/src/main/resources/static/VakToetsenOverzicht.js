d07ca7d2a525747d3c5d13cd4bbdf6dd61cf4988
vsvlak <vsvlak@gmail.com>
2020-08-10 16:48:38 +0200

    vakToetsOverzicht

--- cijferSysteem/src/main/resources/static/VakToetsenOverzicht.js
+++ cijferSysteem/src/main/resources/static/VakToetsenOverzicht.js
@@ -1,16 +1,13 @@
-/*function maakVakkenDropDown(){
+function getToetsById(id){
     let xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function () {
         if (xhr.readyState == 3) {
-            var info = JSON.parse(this.responseText);
-            for (var x = 0; x < info.length; x++) {
-                document.getElementById("kiesvak").innerHTML += "<option>" + info[x].id + ". " + info[x].naam +"</option>";
-            }
+            return this.responseText;
         }
     }
-    xhr.open("GET", "http://localhost:8082/vakkenOverzicht", true);
+    xhr.open("GET", "http://localhost:8082/toets/"+id);
     xhr.send();
-}*/
+}
 
 
 function toonToetsen(vakid){
@@ -22,21 +19,11 @@ function toonToetsen(vakid){
             var info = JSON.parse(this.responseText);
             for (var x = 0; x < info.length; x++) {
                 document.getElementById("tabel").innerHTML += 
-                "<tr><td>" + info[x].naam + "</td>";
+                "<tr><td>" + info[x].datum + "</td>" +
+                "<td>" + info[x].tijd + "</td></tr>";
             }
         }
     }
     xhr.open("GET", "http://localhost:8082/toetsenVanVak/"+vakid, true);
     xhr.send();
 }
-
-/*function getToetsById(id){
-    let xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState == 3) {
-            return this.responseText;
-        }
-    }
-    xhr.open("GET", "http://localhost:8082/toets/"+id);
-    xhr.send();
-}*/

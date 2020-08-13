58aa43c7a333d340ec5cbb90f687d779578cccac
Lisia <lisia.smienk@ziggo.nl>
2020-08-13 12:23:14 +0200

    voorlopige update

--- cijferSysteem/src/main/java/com/cs/cijferSysteem/domein/LeerlingCijfersVanToetsIds.java
+++ cijferSysteem/src/main/java/com/cs/cijferSysteem/domein/LeerlingCijfersVanToetsIds.java
@@ -1,18 +1,24 @@
 package com.cs.cijferSysteem.domein;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 
 import com.cs.cijferSysteem.controller.LeerlingService;
 import com.cs.cijferSysteem.controller.ToetsService;
 
 public class LeerlingCijfersVanToetsIds {
 	Long leerlingid;
-	List<Float> cijfers;
+	List<Float> cijfers = new ArrayList<Float>();
 	
+	@Autowired
 	ToetsService ts;
+	@Autowired
 	LeerlingService ls;
 	
 	public LeerlingCijfersVanToetsIds(Leerling leerling, Iterable<Toets> toetsen){
+		System.out.println("Object aanmaken");
 		this.leerlingid = leerling.getId();
 		for(Toets t:  toetsen) {
 			List<Cijfer> cijfers = t.getCijfers();
@@ -20,6 +26,7 @@ public class LeerlingCijfersVanToetsIds {
 			for(Cijfer c : cijfers) {
 				if(c.getLeerling().getId() == leerlingid) {
 					this.cijfers.add(c.getCijfer());
+					System.out.println(this.cijfers);
 					gevonden = true;
 					break;
 				}
--- cijferSysteem/src/main/java/com/cs/cijferSysteem/rest/ToetsEndpoint.java
+++ cijferSysteem/src/main/java/com/cs/cijferSysteem/rest/ToetsEndpoint.java
@@ -1,18 +1,15 @@
 package com.cs.cijferSysteem.rest;
 
-
 import com.cs.cijferSysteem.controller.DocentService;
 import com.cs.cijferSysteem.controller.KlasService;
 import com.cs.cijferSysteem.controller.ToetsService;
 import com.cs.cijferSysteem.controller.VakService;
-import com.cs.cijferSysteem.domein.Cijfer;
 import com.cs.cijferSysteem.domein.Docent;
 import com.cs.cijferSysteem.domein.Klas;
 import com.cs.cijferSysteem.domein.Leerling;
 import com.cs.cijferSysteem.domein.LeerlingCijfersVanToetsIds;
 import com.cs.cijferSysteem.domein.Toets;
 import com.cs.cijferSysteem.domein.Vak;
-import com.cs.cijferSysteem.dto.CreateLeerlingDto;
 import com.cs.cijferSysteem.dto.CreateToetsDto;
 import com.cs.cijferSysteem.dto.DocentCijferOverzichtDto;
 
@@ -82,20 +79,15 @@ public class ToetsEndpoint {
     @GetMapping("toonToetsenVan/{docentid}/{vakid}/{klasid}")
     public DocentCijferOverzichtDto docentCijferOverzicht(@PathVariable("docentid") Long docentid, @ PathVariable("vakid") Long vakid, @PathVariable("klasid") Long klasid) {
     	DocentCijferOverzichtDto dco = new DocentCijferOverzichtDto();
-    	List<LeerlingCijfersVanToetsIds> list = new ArrayList<>();
+    	List<LeerlingCijfersVanToetsIds> list = new ArrayList<LeerlingCijfersVanToetsIds>();
     	Klas k = ks.getKlasById(klasid).get();
     	List<Leerling> leerlingen = k.getLeerlingen();
     	Iterable<Toets> toetsen = ts.toonToetsenVan(docentid, vakid);
     	for(Leerling l : leerlingen) {
-    		LeerlingCijfersVanToetsIds lc = new LeerlingCijfersVanToetsIds(l, toetsen);
-    		dco.getList().add(lc);
+    		list.add(new LeerlingCijfersVanToetsIds(l, toetsen));
     	}
+    	dco.setList(list);
+    	System.out.println(dco + " " + dco.getList().size());
     	return dco;
-    }
-    
-    
-    
-    
-    
-    
+    }    
 }
